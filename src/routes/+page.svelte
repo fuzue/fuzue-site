fe02f4c2c02ea8de9ad0e38e44711a510150096e
Eduardo Liron <eduardoliron@gmail.com>
2023-05-24 15:07:12 +0200

    feat(scrllSection): add basic scrolling control functions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
+	import ScrollSection from "$lib/scrollSection.svelte";
 
+	let top = 0;
+
+	function manageScroll() {
+		top = window.pageYOffset;
+	}
 </script>
 
-lalala
+<svelte:window on:scroll={manageScroll} />
+
+<div class="h-[200vh]">
+  <ScrollSection {top} />
+  <ScrollSection {top}>
+    <div class="h-[300vh] bg-red-400">
+      
+    </div>
+  </ScrollSection>
+  <ScrollSection {top} />
+</div>
