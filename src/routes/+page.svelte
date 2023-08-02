d51e6c44398fcf76db0712a6afd6a7f9d259487a
Elena Volpato <talkto@elenavolpato.me>
2023-08-02 18:52:00 +0200

    replaced setInterval for resquestAnimationFrame

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,33 +3,40 @@
 	import { onMount } from 'svelte';
 
 	let changingLogo: ChangingLogo;
-  let randomColor = Math.floor(Math.random() * 360);
+	let randomNum = Math.floor(Math.random() * 360);
 
-	onMount(() => manageImages());
-  onMount(() => changeBgColor());
+	onMount(() => window.requestAnimationFrame(animate));
 
-	function manageImages() {
-		setInterval(function () {
+	let prevTimeStampLogo: number, prevTimeStampColor: number;
+
+		function animate(timeStamp: number) {
+		if (prevTimeStampColor === undefined) {
+			prevTimeStampColor = timeStamp;
+		}
+		if (timeStamp >= prevTimeStampColor + 1200) {
+			randomNum = Math.floor(Math.random() * 360);
+			prevTimeStampColor = timeStamp
+		}
+		if (prevTimeStampLogo === undefined) {
+			prevTimeStampLogo = timeStamp;
+		}
+		if (timeStamp >= prevTimeStampLogo + 500) {
 			changingLogo.getRandomLogo();
-		}, 500);
+			prevTimeStampLogo = timeStamp
+		}
+		window.requestAnimationFrame(animate);
 	}
-
-  function changeBgColor () {
-    setInterval(function () {
-			randomColor = Math.floor(Math.random() * 360);
-		}, 1200);
-  }
+	
 </script>
 
-<div class="h-[150vh]" style:background-color="hsl({randomColor}, 100%, 40%)">
+
+<div class="h-[150vh]" style:background-color="hsl({randomNum}, 100%, 40%)">
 	<div class="sticky h-[100vh] top-0 flex justify-center p-[10%]">
-		
 		<ChangingLogo bind:this={changingLogo} />
 		<div class="absolute bottom-10 text-slate-950 font-sans text center flex flex-col items-center">
 			<p class="text-3xl">fuzue@fuzue.tech</p>
 			<p class="text-xl">site coming soon</p>
-			
 		</div>
 	</div>
 </div>
-<!-- <div class="h-[200vh] bg-red-500 -mt-[50vh]">xxx</div> -->
+
