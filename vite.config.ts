cae375f3c784ccc6e8c3e044fadc2379ce38f376
Elena Volpato <talkto@elenavolpato.me>
2023-08-02 11:40:08 +0200

    added vite config to be able to see the site via IP

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	base: `/`,
+  server: {
+    host: '0.0.0.0'
+  },
+  build: {
+    outDir: 'build',
+  },
 });
