bccd852396b7c995f214869582e0017aa6f47f43
Eduardo Liron <eduardoliron@gmail.com>
2023-09-05 16:04:15 -0300

    feat(HowSection): add typing effect

--- src/lib/HowSection.svelte
+++ src/lib/HowSection.svelte
@@ -1,20 +1,42 @@
 <script lang="ts">
-	import { claims } from "./data";
+	import { claims } from './data';
 
-  export let windowPercent: number;
-  let claim = 0
+	export let windowPercent: number;
+	let currentClaim = 0;
+  let currentText = '';
+  let startingFrame = 0;
 
-  $: windowPercent, changeClaim()
+	$: windowPercent, manageScroll();
 
-  function changeClaim() {
-    claim = Math.floor(windowPercent / .25)
-    while(claim > 2) claim = claim - 3
+	function manageScroll() {
+		let newClaim = Math.floor(windowPercent / 0.25);
+		while (newClaim > 2) newClaim = newClaim - 3;
+    if(currentClaim !== newClaim) changeClaim(newClaim)
+	}
+
+  function changeClaim(newClaim: number) {
+    currentClaim = newClaim;
+    currentText = '';
+    startingFrame = 0;
+    if(!window) return;
+    window.requestAnimationFrame(writeClaim)
   }
+
+  function writeClaim(frame: number) {
+    if(currentText.length >= claims[currentClaim].length) return
+    if(startingFrame === 0 || frame - startingFrame >= 100) {
+      startingFrame = frame
+      currentText = claims[currentClaim].substring(0, currentText.length + 1)
+    }
+    console.log(frame, startingFrame, frame - startingFrame, currentText)
+    window.requestAnimationFrame(writeClaim)
+  }
+
 </script>
 
 <section class="h-[200vh] flex justify-center -mb-[50vh]">
-  <div class="sticky top-0 h-screen flex flex-col justify-center text-4xl w-96 max-w-full">
-    <div>We are</div>
-    <div class="ml-auto">{claims[claim]}</div>
-  </div>
+	<div class="sticky top-0 h-screen flex flex-col justify-center text-4xl w-96 max-w-full">
+		<div>We are</div>
+		<div class="ml-auto text-right whitespace-nowrap">{currentText}</div>
+	</div>
 </section>
