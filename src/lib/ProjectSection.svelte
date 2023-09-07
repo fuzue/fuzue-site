1178fb2b2c1154e0fb160e78fced7ecf2c8f6fdc
Eduardo Liron <eduardoliron@gmail.com>
2023-09-07 15:37:30 -0300

    wip(projectSection): sketch basic showcase

--- src/lib/ProjectSection.svelte
+++ src/lib/ProjectSection.svelte
@@ -1,24 +1,15 @@
 <script lang="ts">
 	import { projects } from "./data";
-
-  export let scrollPosition: number;
-  let section: HTMLElement;
-
-  $: animationPos = scrollPosition - Math.floor(scrollPosition);
-  $: currentPos = 1.5+Math.sin((animationPos-.25)*2*Math.PI)
 </script>
 
-<section bind:this={section} style="height: {projects.length * 100}vh;">
-  <div class="sticky top-0 w-full h-screen flex flex-col justify-center items-center text-4xl overflow-hidden">
-    <div
-      class="absolute w-2/3 h-2/3 bg-gray-500"
-      style="
-        width: max(min({currentPos*100}%, 100%), 50%);
-        height: max(min({currentPos*100}%, 100%), 50%);
-        transform: skew(min({currentPos-2}deg, 0deg), min({currentPos-2}deg, 0deg))
-      ">
-      {currentPos} / {animationPos}
-
-    </div>
+{#each projects as project}
+<section
+  class="h-[200vh]">
+  <div class="sticky h-screen top-0 bg-cover flex flex-col p-14 pt-32"
+    style="background-image: url(/imgs/{project.bg}); background-opacity: .3">
+    <h1 class="text-4xl">
+      {project.name}
+    </h1>
   </div>
 </section>
+{/each}
